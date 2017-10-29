c649c8eba514006b191ca3585b218e303ded330a
Tomasz Szczerba <t.szczerba@hotmail.com>
2017-10-29 14:49:44 +0100

    update

--- util/make.sh
+++ util/make.sh
@@ -1,1 +1,1 @@
-python create_repository.py --compressed https://github.com/LightberryEu/plugin.program.hyperion.screenshot https://github.com/tomaszszczerba/plugin.program.hyperion.configurator.git#develop
+python create_repository.py https://github.com/LightberryEu/plugin.program.hyperion.screenshot https://github.com/tomaszszczerba/plugin.program.hyperion.configurator.git#develop
